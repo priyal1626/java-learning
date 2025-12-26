f03b4a91f9661c2d913e9139a49f58729a665cd4
PRIYAL SHAH <priyalshah1626@gmail.com>
2025-12-26 09:23:16 +0530

    Added even odd number program

---
+++ conditions/EvenOdd.java
@@ -0,0 +1,29 @@
+package p1;
+import java.util.Scanner;
+
+public class variabledemo {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter a :");
+		int a=sc.nextInt();
+		
+		if(a%2==0) {
+			System.out.println("number is even");
+		}
+		else {
+			
+			System.out.println("number is odd");
+		}
+		
+			}		
+			
+}
+		
+		
+		
+		
+		
+
+
