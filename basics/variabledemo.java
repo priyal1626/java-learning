5305353cf118a77c0764c1a05fea26f737c66372
PRIYAL SHAH <priyalshah1626@gmail.com>
2025-12-26 10:01:10 +0530

    Created variabledemo

---
+++ basics/variabledemo.java
@@ -0,0 +1,15 @@
+package p1;
+
+public class variabledemo {
+
+	public static void main(String[] args) {
+		int age=19;
+		float money=2.23f;
+		String name="priyal";
+		
+		System.out.println("hello ! my name is "+name+ ", i am "+age+" years old");
+		System.out.println("my pocket money is "+money);
+
+	}
+
+}
